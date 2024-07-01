7591e07fec343a9492f1c670307819a1d3dc22f4
nooblette <min9hyuk@gmail.com>
2024-07-02 06:33:19 +0900

    feat : sec 10. 스프링 트랜잭션 전파1 - 기본 - 스프링 트랜잭션 전파6 - 내부 롤백

--- springtx/src/test/java/hello/springtx/propagation/BasicTxTest.java
+++ springtx/src/test/java/hello/springtx/propagation/BasicTxTest.java
@@ -1,5 +1,7 @@
 package hello.springtx.propagation;
 
+import static org.assertj.core.api.Assertions.*;
+
 import javax.sql.DataSource;
 
 import org.junit.jupiter.api.Test;
@@ -10,6 +12,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.UnexpectedRollbackException;
 import org.springframework.transaction.interceptor.DefaultTransactionAttribute;
 
 import lombok.extern.slf4j.Slf4j;
@@ -117,5 +120,23 @@ public class BasicTxTest {
 		// 외부 트랜잭션은 롤백된다.
 		log.info("외부 트랜잭션 롤백");
 		txManager.rollback(outer);
+	}
+
+	@Test
+	void inner_rollback() {
+		log.info("외부 트랜잭션 시작");
+		TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+
+		log.info("내부 트랜잭션 시작");
+		TransactionStatus inner = txManager.getTransaction(new DefaultTransactionAttribute());
+
+		// 내부 트랜잭션은 롤백된다.
+		log.info("내부 트랜잭션 롤백");
+		txManager.rollback(inner);
+
+		// 외부 트랜잭션은 커밋된다.
+		log.info("외부 트랜잭션 커밋");
+		assertThatThrownBy(() -> txManager.commit(outer))
+			.isInstanceOf(UnexpectedRollbackException.class);
 	}
 }
