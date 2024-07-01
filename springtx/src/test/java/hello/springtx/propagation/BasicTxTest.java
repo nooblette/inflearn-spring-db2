378da58121f1921c3bdeb263c3e1d98a7487165c
nooblette <min9hyuk@gmail.com>
2024-07-02 07:17:19 +0900

    feat : sec 10. 스프링 트랜잭션 전파1 - 기본 - 스프링 트랜잭션 전파7 - REQUIRES_NEW

--- springtx/src/test/java/hello/springtx/propagation/BasicTxTest.java
+++ springtx/src/test/java/hello/springtx/propagation/BasicTxTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.UnexpectedRollbackException;
 import org.springframework.transaction.interceptor.DefaultTransactionAttribute;
@@ -138,5 +139,29 @@ public class BasicTxTest {
 		log.info("외부 트랜잭션 커밋");
 		assertThatThrownBy(() -> txManager.commit(outer))
 			.isInstanceOf(UnexpectedRollbackException.class);
+	}
+
+	@Test
+	void inner_rollback_requires_new() {
+		log.info("외부 트랜잭션 시작");
+		TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+		log.info("outer.isNewTransaction()={}", outer.isNewTransaction()); // true
+
+		log.info("내부 트랜잭션 시작");
+		DefaultTransactionAttribute definition = new DefaultTransactionAttribute();
+
+		// 트랜잭션 전파 옵션을 설정(default : PROPAGATION_REQUIRED, 기존 트랜잭션이 존재할 경우 기존 트랜잭션에 참여)
+		// PROPAGATION_REQUIRES_NEW : 기존 트랜잭션이 있어도 무시하고 신규 물리 트랜잭션을 생성
+		definition.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
+		TransactionStatus inner = txManager.getTransaction(definition);
+		log.info("inner.isNewTransaction()={}", inner.isNewTransaction()); // true
+
+		// 내부 트랜잭션은 롤백된다.
+		log.info("내부 트랜잭션 롤백");
+		txManager.rollback(inner);
+
+		// 외부 트랜잭션은 커밋된다.
+		log.info("외부 트랜잭션 커밋");
+		txManager.commit(outer);
 	}
 }
